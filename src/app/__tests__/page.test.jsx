0cf2c7747638393fe5e4b15b289eb48243d8fa00
dario.murillo <dario.murillo@ucr.ac.cr>
2025-05-21 22:42:32 -0600

    add: tests for homepages

--- src/app/__tests__/page.test.jsx
+++ src/app/__tests__/page.test.jsx
@@ -2,7 +2,29 @@ import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Home from '../page';
 
-test('Page', () => {
+test('carga Wordle heading', () => {
   render(<Home />);
-  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined();
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Wordle' }),
+  ).toBeInTheDocument();
+});
+
+test('carga imagen logo', () => {
+  render(<Home />);
+  expect(screen.getByAltText('Wordle logo')).toBeInTheDocument();
+});
+
+test('carga texto de descripcion', () => {
+  render(<Home />);
+  expect(
+    screen.getByText(/Adivina la palabra de 5 letras en 6 intentos/i),
+  ).toBeInTheDocument();
+});
+
+test('carga ambos botones', () => {
+  render(<Home />);
+  expect(
+    screen.getByRole('button', { name: /Iniciar Sesión/i }),
+  ).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /Jugar/i })).toBeInTheDocument();
 });
